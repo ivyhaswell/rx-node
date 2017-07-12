d332637f093ca455d4332ee2987fe8733abe85f5
shuwen.wang <shuwen.wang@qlchat.com>
2017-07-12 08:50:43 +0800

    添加基础的extend * 生成md5的md5 * 将字符串格式的时间转换为秒数的date2Second

---
+++ src/app/extend/md5/index.ts
@@ -0,0 +1,16 @@
+import { createHash } from 'crypto'
+
+/**
+ * 生成md5
+ * 
+ * @param {(string | Buffer)} data - 数据 
+ * @param {string} [algo='md5'] - 算法，默认使用md5
+ * @returns {string} - md5值
+ */
+function md5(data: string | Buffer, algo: string = 'md5'): string {
+    const md5 = createHash(algo)
+    md5.update(data)
+    return md5.digest('hex')
+}
+
+export default md5
---
+++ src/app/service/html-processor/html-processor.ts
@@ -0,0 +1,4 @@
+import * as fs from 'fs'
+import * as _ from 'underscore'
+import md5 from '../../extend/md5'
+
